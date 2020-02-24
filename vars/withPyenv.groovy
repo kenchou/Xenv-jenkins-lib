5011fc19b8f1560abf48249174d6157e35e24a36
Ken Chou <zhang-j@klab.com>
2020-02-24 20:48:28 +0800

    try to fix expected to call info.pedrocesar.utils.installVersion but wound up catching sh

--- vars/withPyenv.groovy
+++ vars/withPyenv.groovy
@@ -6,9 +6,8 @@ import groovy.transform.Field
 
 
 def call(String version='3.7.0', method=null, cl) {
-  def utils = new info.pedrocesar.utils()
-
   print "Setting up Python version ${version}!"
+  def utils = new info.pedrocesar.utils()
 
   if (0 != sh (returnStatus: true, script: "which ${metaRunner}") && !fileExists("${metaRunnerRoot}/bin/${metaRunner}")) {
     installPyenv()
@@ -16,7 +15,7 @@ def call(String version='3.7.0', method=null, cl) {
 
   if (!fileExists("${metaRunnerRoot}/versions/${version}/")) {
     withEnv(["PATH=${metaRunnerRoot}/bin/:$PATH", "PYENV_ROOT=${metaRunnerRoot}"]) {
-      utils.installVersion(metaRunner, version)
+      installVersion(metaRunner, version)
     }
   }
 
@@ -30,15 +29,25 @@ def call(String version='3.7.0', method=null, cl) {
     withEnv(["PATH=${metaRunnerRoot}/bin/:$PATH", "PYENV_ROOT=${metaRunnerRoot}"]) {
       utils.deleteVersion(metaRunnerRoot, version)
     }
-  } 
+  }
 }
+
 
 def installPyenv() {
   print "Installing ${metaRunner}"
-  new info.pedrocesar.utils().installMetaRunner(metaRunner, metaRunnerRoot)
+  installMetaRunner(metaRunner, metaRunnerRoot)
 }
 
-def purgeAll() {
-  print "Removing all versions of ${metaRunner}"
-  new info.pedrocesar.utils().purgeAllVersions(metaRunnerRoot)
+
+def installMetaRunner(String metaRunner, String metaRunnerRoot){
+  sh """
+  git clone https://github.com/${metaRunner}/${metaRunner}.git ${metaRunnerRoot}
+  cd "${metaRunnerRoot}"
+  src/configure --without-ssl && make -C src
+  """
+}
+
+
+def installVersion(metaRunner, version) {
+  sh "${metaRunner} install ${version}"
 }
